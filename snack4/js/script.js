c9c25182bb8e46af3214be2fb1249957e8842ec7
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-12-06 11:32:06 +0100

    Add new array with mammiferi

---
+++ snack4/js/script.js
@@ -0,0 +1,30 @@
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+// Es:
+
+// [
+//   { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+//   { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+//   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+// ]
+
+// Crea un nuovo array con la lista dei mammiferi.
+// Bonus 1 : trasforma ogni valore di ogni animale in tutto maiuscolo tranne la prima lettera, tipo // leone => lEONE, canidi => cANIDI
+
+
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: 'volpe', famiglia: 'canidi', classe: 'mammiferi'},
+    { nome: 'daino', famiglia: 'cervide', classe: 'mammiferi' },
+    { nome: 'tasso', famiglia: 'canide', classe: 'mammiferi' },
+    { nome: 'procione', famiglia: 'canide', classe: 'mammiferi' },
+    { nome: 'pipistrello', famiglia: 'Vespertilionidae', classe: 'chirottero' },
+    { nome: 'arvicola', famiglia: 'cricetidi', classe: 'roditori' },
+    { nome: 'scoiattolo', famiglia: 'sciuridi', classe: 'roditori' },
+];
+
+const mammiferi = animals.filter((animal) => animal.classe === 'mammiferi');
+
+console.log(mammiferi);
